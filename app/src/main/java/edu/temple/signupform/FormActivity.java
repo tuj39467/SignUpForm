07be83c1ef74e4856e4f1ad4fe351ceefd7cec7c
Christine Gregotski <tuj39467@temple.edu>
2020-09-18 19:27:25 -0400

    Each textView matches its specific purpose (i.e. password EditText and confirm password EditText blur out the text when user inputs some text, email EditText includes the "@"). The "register" button displays three different messages. When a text box is left blank, it informs the user that "information is missing", when the passwords do not match, and when the user registers successfully.

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -1,8 +1,12 @@
 package edu.temple.signupform;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.TextView;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                EditText userName = findViewById(R.id.username);
+                EditText pass = findViewById(R.id.password);
+                EditText confirmPass = findViewById(R.id.confirmPassword);
+                EditText emailAdd = findViewById(R.id.emailAddress);
+
+                String user = userName.getText().toString();
+                String passW = pass.getText().toString();
+                String cpass = confirmPass.getText().toString();
+                String email = emailAdd.getText().toString();
+
+
+                if((user.isEmpty() || email.isEmpty() || cpass.isEmpty() || passW.isEmpty()) || (passW.isEmpty() && !cpass.isEmpty()) || (!passW.isEmpty() && cpass.isEmpty())){
+                    Toast.makeText(FormActivity.this, "Information is missing.",Toast.LENGTH_SHORT).show();
+                }
+                else if(!cpass.equals(passW)){
+                    Toast.makeText(FormActivity.this, "Passwords do not match.",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(FormActivity.this, "Success!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
